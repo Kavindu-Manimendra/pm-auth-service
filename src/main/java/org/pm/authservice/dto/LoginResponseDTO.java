dd75a60cf7ca0dbf2f628b3a22c878208f4cdf7b
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2025-09-29 22:37:03 +0530

    model and dtos added

---
+++ src/main/java/org/pm/authservice/dto/LoginResponseDTO.java
@@ -0,0 +1,10 @@
+package org.pm.authservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class LoginResponseDTO {
+    private String token;
+}
